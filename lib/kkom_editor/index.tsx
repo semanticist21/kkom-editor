507c7ed02d9d57f9775a64d91f1d40ef7acd6345
jiwon <semanticist0@gmail.com>
2024-02-19 23:33:27 +0900

    📱

--- lib/kkom_editor/index.tsx
+++ lib/kkom_editor/index.tsx
@@ -1,5 +1,19 @@
+import "./index.css";
+
+// editor
+import { useEditor, EditorContent, Editor, BubbleMenu } from "@tiptap/react";
+
+// extensions
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+
 const KkomEditor = () => {
-  return <div></div>;
+  const editor = useEditor({
+    extensions: [Document, Paragraph, Text],
+  });
+
+  return <div className="tw-text-red-500">반갑소춘배요</div>;
 };
 
 export default KkomEditor;
